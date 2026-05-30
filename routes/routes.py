423aa57b1dbb9e5d19762a163787395b93823d34
imenbouhrizi <imenbouhrizi@gmail.com>
2026-05-31 00:12:51 +0100

    Imen:modification routes.py

--- routes/routes.py
+++ routes/routes.py
@@ -6,6 +6,8 @@ from app.controllers.auth_controller import auth_bp
 from app.controllers.dashboard_controller import dashboard_bp
 from app.controllers.transaction_controller import transaction_bp
 from app.controllers.budget_controller import budget_bp
+from app.controllers.shared_budget_controller import shared_budget_bp
+from app.controllers.notification_controller import notification_bp
 from app.controllers.goal_controller import goal_bp   # ⚠️ doit exister dans goal_controller.py
 from app.controllers.settings_controller import settings_bp
 from app.controllers.user_controller import user_bp
@@ -30,6 +32,8 @@ def register_routes(app):
 
     # Budgets
     app.register_blueprint(budget_bp, url_prefix="/budgets")
+    app.register_blueprint(shared_budget_bp)
+    app.register_blueprint(notification_bp)
 
     # Objectifs financiers
     app.register_blueprint(goal_bp, url_prefix="/goals")
